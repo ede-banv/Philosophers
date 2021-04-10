803f323f38bc2a1dacebe60ae29acc3c7f90bfea
Emma DE BANVILLE <ede-banv@e1r10p12.42.fr>
2021-04-10 16:08:40 +0200

    push w tester and change from is dead to dies

--- philo_one/ph_stop.c
+++ philo_one/ph_stop.c
@@ -6,7 +6,7 @@
 /*   By: ede-banv <ede-banv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/05 14:23:55 by ede-banv          #+#    #+#             */
-/*   Updated: 2021/04/10 14:43:32 by ede-banv         ###   ########.fr       */
+/*   Updated: 2021/04/10 15:58:25 by ede-banv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int		philo_dead(t_philo *philo, int i)
 	if (!(time_ms() - philo->tolm < (unsigned)g_all->time_to_die))
 	{
 		if (i & 1)
-			printf_lock("is dead", philo);
+			printf_lock("died", philo);
 		return (0);
 	}
 	if ((i & 2) && (g_all->ntepme != -1 && g_all->eat_enough >= g_all->nb_philo))
