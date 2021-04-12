1f223a72db671cd0d60ab6688a127a52f74043cd
Emma DE BANVILLE <ede-banv@e2r4p10.42.fr>
2021-04-12 17:12:42 +0200

    philo two tsrat, to watch videos

---
+++ philo_two/elements.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   elements.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ede-banv <ede-banv@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/12 16:35:44 by ede-banv          #+#    #+#             */
+/*   Updated: 2021/04/12 16:45:43 by ede-banv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __ELEMENTS_H__
+# define __ELEMENTS_H__
+
+# include <pthread.h>
+# include <semaphore.h>
+
+typedef struct	s_philo
+{
+	int		n;
+
+}				t_philo;
+
+#endif
---
+++ philo_two/philo.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ede-banv <ede-banv@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/12 16:34:43 by ede-banv          #+#    #+#             */
+/*   Updated: 2021/04/12 16:44:50 by ede-banv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __PHIlO_H__
+# define __PHILO_H__
+
+# include <string.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include "elements.h"
+
+t_all *g_all;
+
+#endif
